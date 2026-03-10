357043aebe0bf2fe90651e02f1a4577e1ec73cb7
Tejal Patil <tejalpatil0112@gmail.com>
2026-03-10 10:42:50 +0530

    Final project update with code and documentation

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Home Page
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# Dashboard Page - Is route ko dhyan se dekho
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
